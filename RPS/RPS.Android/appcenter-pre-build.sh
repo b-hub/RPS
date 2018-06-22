1187d0060ab9726e45562a1294914b69c86a58cc
b-hub <ben.magistris@gmail.com>
2018-06-22 19:08:16 +0100

    use example bash script with UTF-8 without BOM encoding

--- RPS/RPS.Android/appcenter-pre-build.sh
+++ RPS/RPS.Android/appcenter-pre-build.sh
@@ -1,18 +1,32 @@
-﻿#!/bin/bash
+#!/usr/bin/env bash
+#
+# For Xamarin Android or iOS, change the version name located in AndroidManifest.xml and Info.plist. 
+# AN IMPORTANT THING: YOU NEED DECLARE VERSION_NAME ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION.
 
-if [ ! -n "$APP_CENTER_KEY" ]
+if [ ! -n "$VERSION_NAME" ]
 then
-    echo "You need define the $APP_CENTER_KEY variable in App Center"
+    echo "You need define the VERSION_NAME variable in App Center"
     exit
 fi
 
-APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/RPS/Constants/AppConstant.cs
+ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/Droid/Properties/AndroidManifest.xml
+INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/iOS/Info.plist
 
-if [ -e "$APP_CONSTANT_FILE" ]
+if [ -e "$ANDROID_MANIFEST_FILE" ]
 then
-    echo "Updating ApiUrl to $APP_CENTER_KEY in AppConstant.cs"
-    sed -i '' 's#AppCenterKey = "[a-z:./]*"#AppCenterKey = "'$APP_CENTER_KEY'"#' $APP_CONSTANT_FILE
+    echo "Updating version name to $VERSION_NAME in AndroidManifest.xml"
+    sed -i '' 's/versionName="[0-9.]*"/versionName="'$VERSION_NAME'"/' $ANDROID_MANIFEST_FILE
 
     echo "File content:"
-    cat $APP_CONSTANT_FILE
+    cat $ANDROID_MANIFEST_FILE
+fi
+
+
+if [ -e "$INFO_PLIST_FILE" ]
+then
+    echo "Updating version name to $VERSION_NAME in Info.plist"
+    plutil -replace CFBundleShortVersionString -string $VERSION_NAME $INFO_PLIST_FILE
+
+    echo "File content:"
+    cat $INFO_PLIST_FILE
 fi
