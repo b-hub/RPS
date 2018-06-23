f33e4f1ddf4ed1629edeefe6cf8fb48c950319aa
b-hub <ben.magistris@gmail.com>
2018-06-23 10:53:10 +0100

    Just have the compromised key in the code for now...

--- RPS/RPS/Constants/AppConstant.cs
+++ RPS/RPS/Constants/AppConstant.cs
@@ -6,6 +6,6 @@ namespace RPS.Constants
 {
     public class AppConstant
     {
-        public const string AppCenterKey = "";
+        public const string AppCenterKey = "android=7d5094f0-9688-4203-8318-d6b91d8b62ce";
     }
 }
