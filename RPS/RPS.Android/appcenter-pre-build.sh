375aae216cc151159a2fe042bec44d100976f9d2
b-hub <ben.magistris@gmail.com>
2018-06-23 10:25:54 +0100

    pre-build script small test

--- RPS/RPS.Android/appcenter-pre-build.sh
+++ RPS/RPS.Android/appcenter-pre-build.sh
@@ -8,29 +8,3 @@ then
     echo "You need define the VERSION_NAME variable in App Center"
     exit
 fi
-
-ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/RPS.Android/Properties/AndroidManifest.xml
-INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/iOS/Info.plist
-
-if [ -e "$ANDROID_MANIFEST_FILE" ]
-then
-    echo "Updating version name to $VERSION_NAME in AndroidManifest.xml"
-    sed -i '' 's/versionName="[0-9.]*"/versionName="'$VERSION_NAME'"/' $ANDROID_MANIFEST_FILE
-
-    echo "File content:"
-    cat $ANDROID_MANIFEST_FILE
-else
-    echo "Unable to find AndroidManifest"
-fi
-
-
-if [ -e "$INFO_PLIST_FILE" ]
-then
-    echo "Updating version name to $VERSION_NAME in Info.plist"
-    plutil -replace CFBundleShortVersionString -string $VERSION_NAME $INFO_PLIST_FILE
-
-    echo "File content:"
-    cat $INFO_PLIST_FILE
-else
-    echo "Unable to find Info.plist"
-fi
